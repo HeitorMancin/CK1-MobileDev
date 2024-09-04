11dd27b0bb39910544f7c3bedfc5bc29518a8bf4
Gabs-Moura <gabrielmourasouza@hotmail.com>
2024-09-03 22:13:34 -0300

    Creating all the files

---
+++ 15_08_24/index.js
@@ -0,0 +1,6 @@
+console.log("Hello World!");
+var nome = "Gabriel";
+var idade = 20;
+var tupla = ["Gabriel", 20];
+console.log("Meu nome é " + nome + " e tenho " + idade + " anos");
+console.log(tupla);
